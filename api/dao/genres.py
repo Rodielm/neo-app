11e449f91f507e85fdd5051ef8bcc81d734bda6a
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2022-08-08 12:47:46 +0200

    added get list of genres movies

--- api/dao/genres.py
+++ api/dao/genres.py
@@ -1,13 +1,15 @@
 from api.data import genres
 from api.exceptions.notfound import NotFoundException
 
+
 class GenreDAO:
     """
     The constructor expects an instance of the Neo4j Driver, which will be
     used to interact with Neo4j.
     """
+
     def __init__(self, driver):
-        self.driver=driver
+        self.driver = driver
 
     """
     This method should return a list of genres from the database with a
@@ -24,14 +26,35 @@ class GenreDAO:
     ]
     """
     # tag::all[]
+
     def all(self):
-        # TODO: Open a new session
-        # TODO: Define a unit of work to Get a list of Genres
-        # TODO: Execute within a Read Transaction
+        # Define a unit of work to Get a list of Genres
+        def get_movies(tx):
+            result = tx.run("""
+                MATCH (g:Genre)
+                WHERE g.name <> '(no genres listed)'
 
-        return genres
+                CALL {
+                    WITH g
+                    MATCH (g)<-[:IN_GENRE]-(m:Movie)
+                    WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL
+                    RETURN m.poster AS poster
+                    ORDER BY m.imdbRating DESC LIMIT 1
+                }
+
+                RETURN g {
+                    .*,
+                    movies: size((g)<-[:IN_GENRE]-(:Movie)),
+                    poster: poster
+                } AS genre
+                ORDER BY g.name ASC            
+                """)
+            return [g.value(0) for g in result]
+        # Open a new session
+        with self.driver.session() as session:
+            # Execute within a Read Transaction
+            return session.read_transaction(get_movies)
     # end::all[]
-
 
     """
     This method should find a Genre node by its name and return a set of properties
@@ -40,10 +63,33 @@ class GenreDAO:
     If the genre is not found, a NotFoundError should be thrown.
     """
     # tag::find[]
-    def find(self, name):
-        # TODO: Open a new session
-        # TODO: Define a unit of work to find the genre by it's name
-        # TODO: Execute within a Read Transaction
 
-        return [g for g in genres if g["name"] == name][0]
+    def find(self, name):
+        # Define a unit of work to find the genre by it's name
+        def find_genre(tx, name):
+            first = tx.run("""
+                MATCH (g:Genre {name: $name})<-[:IN_GENRE]-(m:Movie)
+                WHERE m.imdbRating IS NOT NULL AND m.poster IS NOT NULL AND g.name <> '(no genres listed)'
+                WITH g, m
+                ORDER BY m.imdbRating DESC
+
+                WITH g, head(collect(m)) AS movie
+
+                RETURN g {
+                    .name,
+                    movies: size((g)<-[:IN_GENRE]-()),
+                    poster: movie.poster
+                } AS genre
+            """, name=name).single()
+
+            if first == None:
+                raise NotFoundException()
+
+            return first.get("genre")
+            # Open a new session
+        with self.driver.session() as session:
+            # Execute within a Read Transaction
+
+            # return [g for g in genres if g["name"] == name][0]
+            return session.read_transaction(find_genre, name)
     # end::find[]
